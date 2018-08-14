f0ba5a7c1d53e766591349db3e466eea9377da22
Shabana Navas <snavas@acromedia.com>
2018-08-14 12:26:06 +0530

    Add more comments.

--- modules/order/src/MigrateExistingOrderProfiles.php
+++ modules/order/src/MigrateExistingOrderProfiles.php
@@ -16,6 +16,14 @@ class MigrateExistingOrderProfiles {
   /**
    * Batch processing callback for migrating order profiles.
    *
+   * Profiles that can be clearly associated only with shipping information or
+   * only with billing information are migrated accordingly, keeping their IDs.
+   *
+   * Profiles that are associated with both shipping and billing information are
+   * handled differently. The original is migrated to be a billing profile. Then
+   * a copy is created that is migrated to be a shipping profile and the order
+   * shipments that use it are updated to use the new one.
+   *
    * @param array $order_ids
    *   An array of order IDs.
    * @param \Drupal\commerce_order\Entity\OrderTypeInterface $order_type
@@ -24,7 +32,8 @@ class MigrateExistingOrderProfiles {
    *   The batch context array.
    */
   public static function migrateProfiles(array $order_ids, OrderTypeInterface $order_type, array &$context) {
-    $message = 'Migrating Order Profiles...';
+    $message = 'Migrating existing order profiles to use split profiles for
+      billing and shipping...';
 
     $results = [];
     foreach ($order_ids as $order_id) {
